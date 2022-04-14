4eac30c0ad72fd948e4631982e696b57007edc45
Zidane Innis <zidaneinnis@gmail.com>
2022-04-14 14:48:23 -0500

    Added lecture comments on Strict mode
    Strict mode will be use on all code going
    forward.

---
+++ starter/script.js
@@ -0,0 +1,25 @@
+/*Strict Mode Lecture
+
+//Always enable strict mode going forward
+
+//strict modes help to show errors in your code to easier avoid them
+
+"use strict"; // strict mode activation
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive")
+
+// const interface = "Audio"; interface is a reserved word
+
+// const private = 444;
+*/
+
+"use strict";
+
+
+
+
+
