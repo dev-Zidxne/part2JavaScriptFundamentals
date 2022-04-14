237f002e6bcfe8d1a6e9ec3d845bc753c0b8f437
Zidane Innis <zidaneinnis@gmail.com>
2022-04-14 15:23:14 -0500

    Functure Lecture
    Did first function lecture and practice.

--- starter/script.js
+++ starter/script.js
@@ -19,6 +19,30 @@ if (hasDriversLicense) console.log("I can drive")
 
 "use strict";
 
+function logger() {
+    console.log("My name is Zidane")
+}
+
+//calling/ running / invoking functions
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
 
 
 
