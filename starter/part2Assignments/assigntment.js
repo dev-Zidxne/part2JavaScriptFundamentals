1e6d514ef68a664cedfc8bb547448ee3096cdb60
Zidane Innis <zidaneinnis@gmail.com>
2022-04-15 14:43:12 -0500

    Added Functions calling Other Functions

--- starter/part2Assignments/assigntment.js
+++ starter/part2Assignments/assigntment.js
@@ -1,83 +1,83 @@
 "use strict";
 
-//JavaScript Fundamentals – Part 2
+// //JavaScript Fundamentals – Part 2
 
-//LECTURE: Functions
-
-
-
-//1. Write a function called 'describeCountry' which takes three parameters:'country', 'population' and 'capitalCity'. Based on this input, thefunction returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'
-
-//2. Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console
-
-// #1--->
-
-function describeCountry(country, population, capitalCity) {
+// //LECTURE: Functions
 
 
-    const countryDescription = `${country} has ${population} people and its capital city is ${capitalCity}.`;
-    return countryDescription;
-}
 
-//# 2--->
-const jamaica = describeCountry("Jamaica", "3 million", "Mobay")
+// //1. Write a function called 'describeCountry' which takes three parameters:'country', 'population' and 'capitalCity'. Based on this input, thefunction returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'
 
-const USA = describeCountry("USA", "300 million", "Washington")
+// //2. Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console
 
-const china = describeCountry("China", "1 billion", "Hong Kong")
+// // #1--->
 
-console.log(jamaica, USA, china)
+// function describeCountry(country, population, capitalCity) {
 
-//LECTURE: Function Declarations vs. Expressions
 
-//1. The world population is 7900 million people. Create a function declarationcalled 'percentageOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population
-
-//3 Call 'percentageOfWorld1' for 3 populations of countries of your choice, store the results into variables, and log them to the console
-
-//4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', and also call it with 3 country populations (can be the same populations)
-
-//Declaration--------------------------------------------------------------
-
-// #1 and #3--->
-// function percentageOfWorld2(population) {
-//     return population / 7900 * 100
+//     const countryDescription = `${country} has ${population} people and its capital city is ${capitalCity}.`;
+//     return countryDescription;
 // }
-// const jamaicaPercentage = percentageOfWorld2(3)
 
-// const americaPercentage = percentageOfWorld2(300)
+// //# 2--->
+// const jamaica = describeCountry("Jamaica", "3 million", "Mobay")
 
-// const chinaPercentage = percentageOfWorld2(1000)
+// const USA = describeCountry("USA", "300 million", "Washington")
 
-// console.log(jamaicaPercentage, americaPercentage, chinaPercentage)
+// const china = describeCountry("China", "1 billion", "Hong Kong")
+
+// console.log(jamaica, USA, china)
+
+// //LECTURE: Function Declarations vs. Expressions
+
+// //1. The world population is 7900 million people. Create a function declarationcalled 'percentageOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population
+
+// //3 Call 'percentageOfWorld1' for 3 populations of countries of your choice, store the results into variables, and log them to the console
+
+// //4. Create a function expression which does the exact same thing, called 'percentageOfWorld2', and also call it with 3 country populations (can be the same populations)
+
+// //Declaration--------------------------------------------------------------
+
+// // #1 and #3--->
+// // function percentageOfWorld2(population) {
+// //     return population / 7900 * 100
+// // }
+// // const jamaicaPercentage = percentageOfWorld2(3)
+
+// // const americaPercentage = percentageOfWorld2(300)
+
+// // const chinaPercentage = percentageOfWorld2(1000)
+
+// // console.log(jamaicaPercentage, americaPercentage, chinaPercentage)
 
 
 
 
-// // //Expression-----------------------------------------------------
+// // // //Expression-----------------------------------------------------
 
-// #4--->
+// // #4--->
 
 
 
-const percentageOfWorld1 = function (population) {
-    return `Your country's population is ${population / 7900 * 100}% of the world population. ` // string can be added to functions using template literal 
-}
-const percentage1 = percentageOfWorld1(3)
+// const percentageOfWorld1 = function (population) {
+//     return `Your country's population is ${population / 7900 * 100}% of the world population. ` // string can be added to functions using template literal 
+// }
+// const percentage1 = percentageOfWorld1(3)
 
-const percentage2 = percentageOfWorld1(300)
+// const percentage2 = percentageOfWorld1(300)
 
-const percentage3 = percentageOfWorld1(1000)
+// const percentage3 = percentageOfWorld1(1000)
 
-console.log("", percentage1, '\n', percentage2, '\n', percentage3) // '\n' can be used to break content into new line on the console :O
+// console.log("", percentage1, '\n', percentage2, '\n', percentage3) // '\n' can be used to break content into new line on the console :O
 
-//LECTURE: Arrow Functions
+// //LECTURE: Arrow Functions
 
-//1. Recreate the last assignment, but this time create an arrow function called'percentageOfWorld3'
+// //1. Recreate the last assignment, but this time create an arrow function called'percentageOfWorld3'
 
-const percentOfWorld3 = population => population / 7900 * 100
-const country1 = percentOfWorld3(3)
+// const percentOfWorld3 = population => population / 7900 * 100
+// const country1 = percentOfWorld3(3)
 
-console.log(country1)
+// console.log(country1)
 
 
 
--- starter/script.js
+++ starter/script.js
@@ -91,6 +91,27 @@ console.log(yearsUntilRetirement(1976, "Chris"));
 
 
 
+//Functions Calling Other Functions
+function cutPieces(fruit) {
+    return fruit * 8;
+}
+
+
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutPieces(apples);
+    const orangePieces = cutPieces(oranges);
+
+    const juice = `Juice with ${applePieces
+        } pieces of apples and ${orangePieces} pieces of oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+
+
 
 
 
