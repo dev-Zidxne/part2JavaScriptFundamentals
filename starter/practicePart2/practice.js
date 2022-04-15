04324b3891640513a07f738e1b67e29e42fcea53
Zidane Innis <zidaneinnis@gmail.com>
2022-04-15 10:00:58 -0500

    Updating Javascript Files
    Added functions practice including
    expressive and declarative functions practice
    
    I am starting get an undestand of using functions now.

---
+++ starter/practicePart2/practice.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Functions Practice------------------
+
+const day1 = dayOfTheWeek("monday")
+
+console.log(day1)
+
+function dayOfTheWeek(day) {
+    const day1 = `The day is ${day}`
+    return day1
+}
+const tuesday = dayOfTheWeek("tuesday")
+
+
+console.log(tuesday)
+
+
+//-----------------------------
+
+
+function dayOfTheWeek(day) {
+    const day4 = `The day is ${day}`
+    return day4
+}
+
+const wednesday = dayOfTheWeek("wednesday")
+
+console.log(wednesday)
+
+//-----------------------------------------
+
+
+
+function monthOfTheYear(month) {
+    const month11 = `The current month of the year is ${month}.`;
+    return month11
+}
+
+const november = monthOfTheYear("november")
+
+console.log(november)
--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*Strict Mode Lecture
 
 //Always enable strict mode going forward
@@ -15,12 +16,12 @@ if (hasDriversLicense) console.log("I can drive")
 // const interface = "Audio"; interface is a reserved word
 
 // const private = 444;
-*/
+
 
 "use strict";
 
 function logger() {
-    console.log("My name is Zidane")
+    console.log("My name is Zidane");
 }
 
 //calling/ running / invoking functions
@@ -32,17 +33,47 @@ logger();
 function fruitProcessor(apples, oranges) {
 
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+    return juice; // return basically puts back the into the variable which is fruitProcessor in this
 }
 
-const appleJuice = fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0); // here we put values into them
 
-console.log(appleJuice);
+console.log(appleJuice); // it is displays full string from fruitProcessor
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
+
+*/
+
+//Function Declaration 
+
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+
+// //Function Expressions
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+// function dayOfTheWeek(day) {
+//     const monday = `The day is ${day}`;
+//     return monday;
+
+// }
+
+
+
+
+
 
 
 
